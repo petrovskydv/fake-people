1acefa987abb38c954272bb16a93c5e5f35ce4f8
Денис Петровский <petrovsky.dv@gmail.com>
2021-01-13 22:36:30 +0300

    step7

--- main.py
+++ main.py
@@ -2,21 +2,38 @@ import file_operations
 from faker import Faker
 import random
 
-fake = Faker("ru_RU")
 
-context = {
-    'first_name': fake.first_name(),
-    'last_name': fake.last_name(),
-    'job': fake.job(),
-    'town': fake.city(),
-    'strength': random.randint(8, 14),
-    'agility': random.randint(8, 14),
-    'endurance': random.randint(8, 14),
-    'intelligence': random.randint(8, 14),
-    'luck': random.randint(8, 14),
-    'skill_1': '',
-    'skill_2': '',
-    'skill_3': ''
-}
+def main():
+    fake = Faker("ru_RU")
 
-file_operations.render_template('charsheet.svg', 'result.svg', context)
+    skills_list = [
+        'Стремительный прыжок',
+        'Электрический выстрел',
+        'Ледяной удар',
+        'Стремительный удар',
+        'Кислотный взгляд',
+        'Тайный побег',
+        'Ледяной выстрел',
+        'Огненный заряд'
+    ]
+
+    context = {
+        'first_name': fake.first_name(),
+        'last_name': fake.last_name(),
+        'job': fake.job(),
+        'town': fake.city(),
+        'strength': random.randint(8, 14),
+        'agility': random.randint(8, 14),
+        'endurance': random.randint(8, 14),
+        'intelligence': random.randint(8, 14),
+        'luck': random.randint(8, 14),
+        'skill_1': random.choice(skills_list),
+        'skill_2': random.choice(skills_list),
+        'skill_3': random.choice(skills_list)
+    }
+
+    file_operations.render_template('charsheet.svg', 'result.svg', context)
+
+
+if __name__ == '__main__':
+    main()
