51e4405a3d573c81c736e2db84a82c1e25fd951f
Денис Петровский <petrovsky.dv@gmail.com>
2021-01-13 22:00:12 +0300

    step2

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import file_operations
 
-print(file_operations.VERSION)
+context = {
+    'first_name': 'Презентация',
+    'last_name': 'на тему',
+    'job': 'Картинки',
+    'town': '',
+    'strength': '',
+    'agility': 'Презентация',
+    'endurance': 'на тему',
+    'intelligence': 'Картинки',
+    'luck': '',
+    'skill_1': '',
+    'skill_2': '',
+    'skill_3': ''
+}
+
+file_operations.render_template('charsheet.svg', 'result.svg', context)
